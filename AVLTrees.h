91ef75fb3d20a1877a28d0330cff471184e46dd7
habibaaymannn <145665685+habibaaymannn@users.noreply.github.com>
2024-05-18 22:27:08 +0300

    Update AVLTrees.h

--- AVLTrees.h
+++ AVLTrees.h
@@ -1,269 +1,236 @@
-#include <bits/stdc++.h>
+// Created by habibaayman on 5/13/2024.
+
+#ifndef CLIONPROJECTS_AVLTREES_H
+#define CLIONPROJECTS_AVLTREES_H
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <fstream>
 #include "Items.h"
-#include "Heap.h"
-#include "AVLTrees.h"
-#include "BST.h"
-using namespace std;
-void readItems(istream& input, vector<Item>& items) {
-    string name, category, priceStr;
-    int price;
-    while (getline(input, name)) {
-        getline(input, category);
-        getline(input, priceStr);
-        if (!name.empty() && !category.empty() && !priceStr.empty()) {
-            stringstream ss(priceStr);
-            ss >> price;
-            items.push_back(Item(name, category, price));
-        }
+template<typename T>
+class AVLTreeNode {
+public:
+    T key;
+    int height;
+    AVLTreeNode<T>* left;
+    AVLTreeNode<T>* right;
+
+    AVLTreeNode(T key, AVLTreeNode<T>* left = nullptr, AVLTreeNode<T>* right = nullptr)
+            : key(key), height(1), left(left), right(right) {}
+};
+template<typename T>
+class AVLTree {
+private:
+    AVLTreeNode<T>* root;
+
+    int height(AVLTreeNode<T>* node) {
+        if (node == nullptr) return 0;
+        int lefth = height(node->left);
+        int righth = height(node->right);
+        return max(lefth, righth) + 1;
     }
-}
-
-
-template<class T>
-void read(istream& input, AVLTree<T>& tree) {
-    int numItems;
-    input >> numItems;
-
-    for (int i = 0; i < numItems; ++i) {
-        string itemName, category;
-        int price;
-
-        input.ignore();
-        getline(input, itemName);
-        getline(input, category);
-        input >> price;
-
-        tree.insert(Item(itemName, category, price));
+    int getBalance(AVLTreeNode<T>* node) {
+        if (node == nullptr) return 0;
+        return height(node->left) - height(node->right);
     }
-}
+    AVLTreeNode<T>* rotateRight(AVLTreeNode<T>* x) {
+        AVLTreeNode<T>* leftX = x->left;//left child of x
+        AVLTreeNode<T>* RleftX = leftX->right;//right child of leftX
 
+        leftX->right = x;
+        x->left = RleftX;
 
+        x->height = max(height(x->left), height(x->right)) + 1;
+        leftX->height = max(height(leftX->left), height(leftX->right)) + 1;
 
-void print_heap(vector<Item>& heap) {
-    for (Item item : heap) {
-        cout << item.itemname << endl;
-        cout << item.category << endl;
-        cout << item.price << endl;
+        return x;
     }
-}
+    AVLTreeNode<T>* rotateLeft(AVLTreeNode<T>* x) {
+        AVLTreeNode<T>* rightX = x->right;//right child of x
+        AVLTreeNode<T>* LrightX = rightX->left;//left child of rightX
 
-void showMenu() {
-    cout << "1. Add item data" << endl;
-    cout << "2. remove item data" << endl;
-    cout << "3. Display the item data normally" << endl;
-    cout << "4. Display all the items sorted by their name ascending" << endl;
-    cout << "5. Display all the items sorted by their name descending" << endl;
-    cout << "6. Display all the items sorted by their prices ascending" << endl;
-    cout << "7. Display all the items sorted by their prices descending" << endl;
-    cout << "0. Exit" << endl;
-    cout << "Please enter your choice: ";
-}
+        rightX->left = x;
+        x->right = LrightX;
 
-int main() {
-    vector<Item> items;
-    ifstream input("C:\\Users\\hhaym\\Documents\\clionprojects\\Data_Structures\\items.txt"); // Open the file
+        x->height = max(height(x->left), height(x->right)) + 1;
+        rightX->height = max(height(rightX->left), height(rightX->right)) + 1;
 
-    readItems(input, items);
-    if (!input.is_open()) {
-        cout << "Failed to open the file." << endl;
-        return 1;
+        return rightX;
     }
 
-    readItems(input, items);
-    cout<<"1-Binary Search Tree (BST)"<<endl;
-    cout<<"2-Heaps"<<endl;
-    cout<<"3-AVL Trees"<<endl;
-    cout<<"please enter the number of the data structure you want:";
-    int ans;
-    cin>>ans;
-    if(ans==1){
-        BST<Item>tree;
-        for(auto i:items){
-            tree.add_item(i);
-        }
-        bool a = 1;
-        while(a){
-            showMenu();
-            int c; cin>>c;
-            if(c==1){
-                Item new_item ;
-                cout<<"Enter Item name: ";
-                cin>>new_item.itemname;
-                cout<<"\nEnter Item category: ";
-                cin>>new_item.category;
-                cout<<"\nEnter Item price: ";
-                cin>>new_item.price;
-                tree.add_item(new_item);
-                items.push_back(new_item);
-            }else if(c==2){
-                Item delete_item ;
-                cout<<"Enter Item name: ";
-                cin>>delete_item.itemname;
-                cout<<"\nEnter Item category: ";
-                cin>>delete_item.category;
-                cout<<"\nEnter Item price: ";
-                cin>>delete_item.price;
-                tree.Remove_item(delete_item);
+    AVLTreeNode<T>* insertNode(AVLTreeNode<T>* node, const T& key) {
+        if (node == nullptr)
+            return new AVLTreeNode<T>(key);
+        if (key < node->key)
+            node->left = insertNode(node->left, key);
+        else if (key > node->key)
+            node->right = insertNode(node->right, key);
+        else
+            return node;
 
-                auto it = std::find(items.begin(), items.end(), delete_item);
-                if (it != items.end()) {
-                    items.erase(it);
-                }
-            }else if(c==3) {
-                for(auto i:items){
-                    i.print();
-                }
-            }else if(c==4){
-                tree.Ascending_Name();
-            }else if(c==5) {
-                tree.Descending_Name();
-            }else if(c==6){
-                tree.Ascending_Price();
-            }else if(c==7){
-                tree.Descending_Price();
-            }else if(c==0){
-                a = 0;
-            }
+        node->height = 1 + max(height(node->left), height(node->right));
+        int balance = getBalance(node);
+
+        // rotate left
+        if (balance > 1 && key < node->left->key)
+            return rotateRight(node);
+
+        // rotate LR
+        if (balance > 1 && key > node->left->key) {
+            node->left = rotateLeft(node->left);
+            return rotateRight(node);
         }
 
+        // rotate right
+        if (balance < -1 && key > node->right->key)
+            return rotateLeft(node);
+
+        // RL rotate
+        if (balance < -1 && key < node->right->key) {
+            node->right = rotateRight(node->right);
+            return rotateLeft(node);
+        }
+        return node;
     }
-    else if (ans == 2) {
-        minHeap minheap;
-        maxHeap maxheap;
-        for (Item& item : items) {
-            minheap.insertPrice(item);
-            maxheap.insertPrice(item);
+    AVLTreeNode<T>* minValueNode(AVLTreeNode<T>* node) {
+        AVLTreeNode<T>* current = node;
+
+        while (current && current->left != nullptr)
+            current = current->left;
+
+        return current;
+    }
+    AVLTreeNode<T>* deleteNode(AVLTreeNode<T>* root, const T& key) {
+        if (root == nullptr) {
+            cout<<"item not found\n";
+            return root;
+        }
+        if (key < root->key)
+            root->left = deleteNode(root->left, key);
+        else if (key > root->key)
+            root->right = deleteNode(root->right, key);
+        else {
+            // Node with only one child or no child
+            if ((root->left == nullptr) || (root->right == nullptr)) {
+                AVLTreeNode<T>* temp = root->left ? root->left : root->right;
+
+                // No child
+                if (temp == nullptr) {
+                    temp = root;
+                    root = nullptr;
+                } else
+                    // One child
+                    *root = *temp;
+
+                delete temp;
+            } else {
+                AVLTreeNode<T>* temp = minValueNode(root->right);
+                root->key = temp->key;
+                root->right = deleteNode(root->right, temp->key);
+            }
+        }
+        if (root == nullptr) return root;
+
+        root->height = 1 + max(height(root->left), height(root->right));
+        int balance = getBalance(root);
+
+        // right rotation
+        if (balance > 1 && getBalance(root->left) >= 0)
+            return rotateRight(root);
+
+        // Left Right rotation
+        if (balance > 1 && getBalance(root->left) < 0) {
+            root->left = rotateLeft(root->left);
+            return rotateRight(root);
         }
 
-        int choice;
-        do {
-            showMenu();
-            cin >> choice;
-            if (choice == 1) {
-                string name, category;
-                int price;
-                cout << "Enter item name: ";
-                cin >> name;
-                cout << "Enter category: ";
-                cin >> category;
-                cout << "Enter price: ";
-                cin >> price;
-                Item newItem(name, category, price);
-                minheap.insertPrice(newItem);
-                maxheap.insertPrice(newItem);
-            }
-            else if (choice == 2) {
-                cout << "1. Remove by name (MinHeap)" << endl;
-                cout << "2. Remove by name (MaxHeap)" << endl;
-                cout << "3. Remove by price (MinHeap)" << endl;
-                cout << "4. Remove by price (MaxHeap)" << endl;
-                int removeChoice;
-                cin >> removeChoice;
-                if (removeChoice == 1) {
-                    minheap.pop_name();
-                }
-                else if (removeChoice == 2) {
-                    maxheap.pop_name();
-                }
-                else if (removeChoice == 3) {
-                    minheap.pop_price();
-                }
-                else if (removeChoice == 4) {
-                    maxheap.pop_price();
-                }
-            }
-            else if (choice == 3) {
-                cout << "Heap items (MinHeap):" << endl;
-                minheap.print_heap();
-                cout << "Heap items (MaxHeap):" << endl;
-                maxheap.print_heap();
-            }
-            else if (choice == 4) {
-                cout << "Items sorted by name ascending (MinHeap):" << endl;
-                minheap.heapSortMinName();
-                minheap.print_heap();
-            }
-            else if (choice == 5) {
-                cout << "Items sorted by name descending (MaxHeap):" << endl;
-                maxheap.heapSortMaxName();
-                maxheap.print_heap();
-            }
-            else if (choice == 6) {
-                cout << "Items sorted by price ascending (MinHeap):" << endl;
-                minheap.heapSortMinPrice();
-                minheap.print_heap();
-            }
-            else if (choice == 7) {
-                cout << "Items sorted by price descending (MaxHeap):" << endl;
+        // left rotation
+        if (balance < -1 && getBalance(root->right) <= 0)
+            return rotateLeft(root);
 
-                maxheap.heapSortMaxPrice();
-                maxheap.print_heap();
-            }
-        } while (choice != 0);
-    }
-    else {
-        AVLTree<Item> avltree;
-        ifstream inputFile("C:\\Users\\hhaym\\Documents\\clionprojects\\Data_Structures\\items.txt");
-        if (!inputFile) {
-            cerr << "Error: Unable to open the file." << endl;
-            return 1;
+        // Right Left rotation
+        if (balance < -1 && getBalance(root->right) > 0) {
+            root->right = rotateRight(root->right);
+            return rotateLeft(root);
         }
-        read(inputFile, avltree);
-        inputFile.close();
-        int choice;
-        do {
-            showMenu();
-            cin >> choice;
-            if (choice == 1) {
-                string name, category;
-                int price;
-                cout << "Enter item name: ";
-                cin >> name;
-                cout << "Enter category: ";
-                cin >> category;
-                cout << "Enter price: ";
-                cin >> price;
-                Item newItem(name, category, price);
-                avltree.insert(newItem);
-            }
-            else if (choice == 2) {
-                string name, category;
-                int price;
-                cout << "Enter item name: ";
-                cin >> name;
-                cout << "Enter category: ";
-                cin >> category;
-                cout << "Enter price: ";
-                cin >> price;
-                avltree.remove(Item(name, category, price));
-            }
-            else if (choice == 3) {
-                cout << "Display Normally:\n";
-                avltree.displaynormal();
-                cout << endl;
-            }
-            else if (choice == 4) {
-                cout << "Sort name in ascending order:\n";
-                avltree.AscendingName();
-                cout << endl;
-            }
-            else if (choice == 5) {
-                cout << "Sort name in descending order:\n";
-                avltree.DescendingName();
-                cout << endl;
-            }
-            else if (choice == 6) {
-                cout << "Sort price in ascending order:\n";
-                avltree.AscendingPrice();
-                cout << endl;
-            }
-            else if (choice == 7) {
-                cout << "Sort price in descending order:\n";
-                avltree.DescendingPrice();
-                cout << endl;
-            }
-        } while (choice != 0);
+        return root;
     }
 
-    return 0;
-}
+    void inorder(AVLTreeNode<T>* root) {
+        if (root != nullptr) {
+            inorder(root->left);
+            cout << root->key.getitemname() << " " << root->key.getPrice() << endl;
+            inorder(root->right);
+        }
+    }
+
+
+    void reverseorder(AVLTreeNode<T>* root) {
+        if (root != nullptr) {
+            reverseorder(root->right);
+            cout << root->key.getitemname() << " " << root->key.getPrice() << endl;
+            reverseorder(root->left);
+        }
+    }
+    //puts them in array
+    void collectItemsinorder(AVLTreeNode<T>* root, vector<T>& items) {
+        if (root != nullptr) {
+            collectItemsinorder(root->left, items);
+            items.push_back(root->key);
+            collectItemsinorder(root->right, items);
+        }
+    }
+
+public:
+    AVLTree() : root(nullptr) {}
+    void insert(const T& key) {
+        root = insertNode(root, key);
+    }
+    void remove(const T& key) {
+        root = deleteNode(root, key);
+    }
+    void displaynormal() {
+        inorder(root);
+    }
+    void AscendingName() {
+        inorder(root);
+    }
+    void DescendingName() {
+        reverseorder(root);
+    }
+    void AscendingPrice() {
+        vector<T> items;
+        collectItemsinorder(root, items);
+        int n = items.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (items[j].getPrice() > items[j + 1].getPrice()) {
+                    T temp = items[j];
+                    items[j] = items[j + 1];
+                    items[j + 1] = temp;
+                }
+            }
+        }
+        for (const auto& item : items) {
+            cout << item.getitemname() << " " << item.getPrice() << endl;
+        }
+    }
+    void DescendingPrice() {
+        vector<T> items;
+        collectItemsinorder(root, items);
+        int n = items.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (items[j].getPrice() < items[j + 1].getPrice()) {
+                    T temp = items[j];
+                    items[j] = items[j + 1];
+                    items[j + 1] = temp;
+                }
+            }
+        }
+        for (const auto& item : items) {
+            cout << item.getitemname() << " " << item.getPrice() << endl;
+        }
+    }
+};
+#endif //CLIONPROJECTS_AVLTREES_H
